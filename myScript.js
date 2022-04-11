3a21ac124fc8d46c54afcd0bb23150658561d448
ishwar4230 <atcikss9@kgpian.iitkgp.ac.in>
2022-04-11 23:30:49 +0530

    1 bug fixed

--- myScript.js
+++ myScript.js
@@ -33,57 +33,53 @@ function checkwin(n1, n2) {
         location.reload();
         alert("Press start button again to start a new Game!");
         return 1;
-    }
-    if (document.getElementById("b4").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b6").innerHTML && (document.getElementById("b4").innerHTML == 'X' || document.getElementById("b4").innerHTML == 'O')) {
+    } else if (document.getElementById("b4").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b6").innerHTML && (document.getElementById("b4").innerHTML == 'X' || document.getElementById("b4").innerHTML == 'O')) {
         if (document.getElementById("b4").innerHTML == "X") {
             alert(n1 + " won the game!");
         } else { alert(n2 + " won the game!"); }
         location.reload();
         alert("Press start button again to start a new Game!");
-    }
-    if (document.getElementById("b7").innerHTML == document.getElementById("b8").innerHTML && document.getElementById("b8").innerHTML == document.getElementById("b9").innerHTML && (document.getElementById("b7").innerHTML == 'X' || document.getElementById("b7").innerHTML == 'O')) {
+    } else if (document.getElementById("b7").innerHTML == document.getElementById("b8").innerHTML && document.getElementById("b8").innerHTML == document.getElementById("b9").innerHTML && (document.getElementById("b7").innerHTML == 'X' || document.getElementById("b7").innerHTML == 'O')) {
         if (document.getElementById("b7").innerHTML == "X") {
             alert(n1 + " won the game!");
         } else { alert(n2 + " won the game!"); }
         location.reload();
         alert("Press start button again to start a new Game!");
-    }
-    if (document.getElementById("b1").innerHTML == document.getElementById("b4").innerHTML && document.getElementById("b4").innerHTML == document.getElementById("b7").innerHTML && (document.getElementById("b1").innerHTML == 'X' || document.getElementById("b1").innerHTML == 'O')) {
+    } else if (document.getElementById("b1").innerHTML == document.getElementById("b4").innerHTML && document.getElementById("b4").innerHTML == document.getElementById("b7").innerHTML && (document.getElementById("b1").innerHTML == 'X' || document.getElementById("b1").innerHTML == 'O')) {
         if (document.getElementById("b1").innerHTML == "X") {
             alert(n1 + " won the game!");
         } else { alert(n2 + " won the game!"); }
         location.reload();
         alert("Press start button again to start a new Game!");
-    }
-    if (document.getElementById("b2").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b8").innerHTML && (document.getElementById("b2").innerHTML == 'X' || document.getElementById("b2").innerHTML == 'O')) {
+    } else if (document.getElementById("b2").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b8").innerHTML && (document.getElementById("b2").innerHTML == 'X' || document.getElementById("b2").innerHTML == 'O')) {
         if (document.getElementById("b2").innerHTML == "X") {
             alert(n1 + " won the game!");
         } else { alert(n2 + " won the game!"); }
         location.reload();
         alert("Press start button again to start a new Game!");
-    }
-
-    if (document.getElementById("b3").innerHTML == document.getElementById("b6").innerHTML && document.getElementById("b6").innerHTML == document.getElementById("b9").innerHTML && (document.getElementById("b3").innerHTML == 'X' || document.getElementById("b3").innerHTML == 'O')) {
+    } else if (document.getElementById("b3").innerHTML == document.getElementById("b6").innerHTML && document.getElementById("b6").innerHTML == document.getElementById("b9").innerHTML && (document.getElementById("b3").innerHTML == 'X' || document.getElementById("b3").innerHTML == 'O')) {
         if (document.getElementById("b3").innerHTML == "X") {
             alert(n1 + " won the game!");
         } else { alert(n2 + " won the game!"); }
         location.reload();
         alert("Press start button again to start a new Game!");
-    }
-
-    if (document.getElementById("b1").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b9").innerHTML && (document.getElementById("b1").innerHTML == 'X' || document.getElementById("b1").innerHTML == 'O')) {
+    } else if (document.getElementById("b1").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b9").innerHTML && (document.getElementById("b1").innerHTML == 'X' || document.getElementById("b1").innerHTML == 'O')) {
         if (document.getElementById("b1").innerHTML == "X") {
             alert(n1 + " won the game!");
         } else { alert(n2 + " won the game!"); }
         location.reload();
         alert("Press start button again to start a new Game!");
-    }
-    if (document.getElementById("b3").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b7").innerHTML && (document.getElementById("b3").innerHTML == 'X' || document.getElementById("b3").innerHTML == 'O')) {
+    } else if (document.getElementById("b3").innerHTML == document.getElementById("b5").innerHTML && document.getElementById("b5").innerHTML == document.getElementById("b7").innerHTML && (document.getElementById("b3").innerHTML == 'X' || document.getElementById("b3").innerHTML == 'O')) {
         if (document.getElementById("b3").innerHTML == "X") {
             alert(n1 + " won the game!");
         } else { alert(n2 + " won the game!"); }
         location.reload();
         alert("Press start button again to start a new Game!");
+    } else {
+        if (i == 10) {
+            alert("No-one won the Game!")
+            location.reload();
+        }
     }
 }
 
@@ -105,19 +101,19 @@ function print(p) {
                 i++;
                 k = checkwin(player1.name, player2.name);
 
-                if (i == 10) {
-                    alert("No-one won the Game!")
-                    location.reload();
-                }
+                // if (i == 10) {
+                //     alert("No-one won the Game!")
+                //     location.reload();
+                // }
             } else {
                 document.getElementById(p).innerHTML = "O";
                 i++;
                 k = checkwin(player1.name, player2.name);
 
-                if (i == 10) {
-                    alert("No-one won the Game!")
-                    location.reload();
-                }
+                // if (i == 10) {
+                //     alert("No-one won the Game!")
+                //     location.reload();
+                // }
             }
 
         } else alert("box already filled!");
